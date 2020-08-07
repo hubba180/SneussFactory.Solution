317f5b876b170af7e0577a4ef069413c073a647f
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-08-07 16:55:06 -0700

    add factorywide details page

--- SneussFactory/Controllers/HomeController.cs
+++ SneussFactory/Controllers/HomeController.cs
@@ -1,23 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
     private readonly SneussFactoryContext _db;
-    // public HomeController(SneussFactoryContext db)
-    // {
-    //   _db = db;
-    // }
+    public HomeController(SneussFactoryContext db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
       return View();
     }
-    // public ActionResult Lists()
-    // {
-    //   return View(_db)
-    // }
+    public ActionResult Lists()
+    {
+      return View(_db);
+    }
 
   }
 }
