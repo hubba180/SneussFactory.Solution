ca86a73a27415e84de8024781e48f5fa39a8d01c
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-08-07 16:38:50 -0700

    add delete engineer button to machines

--- SneussFactory/Controllers/HomeController.cs
+++ SneussFactory/Controllers/HomeController.cs
@@ -4,10 +4,20 @@ namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly SneussFactoryContext _db;
+    // public HomeController(SneussFactoryContext db)
+    // {
+    //   _db = db;
+    // }
     [HttpGet("/")]
     public ActionResult Index()
     {
       return View();
     }
+    // public ActionResult Lists()
+    // {
+    //   return View(_db)
+    // }
+
   }
 }
